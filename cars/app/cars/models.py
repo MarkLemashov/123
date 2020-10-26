db2262837d64f4e53778e3c46cfcea3c6afbce75
MarkLemashov <marklemashov049@gmail.com>
2020-10-26 23:11:56 +0200

    add customer model

--- cars/app/cars/models.py
+++ cars/app/cars/models.py
@@ -1,8 +1,22 @@
 from django.db import models
 
 
+class Customer(models.Model):
+    name = models.CharField(max_length=60)
+    money = models.IntegerField(default=0)
+
+    def buy_car(self, car_id):
+        car = Car.objects.get(id=car_id)
+        car.customer = self;
+        car.save()
+
+    def __str__(self):
+        return self.name
+
+
 class Car(models.Model):
     make = models.CharField(max_length=60)
+    customer = models.ForeignKey(Customer, on_delete=models.CASCADE, null=True, blank=True)
 
     def __str__(self):
         return self.make
--- cars/app/cars/views.py
+++ cars/app/cars/views.py
@@ -1,9 +1,16 @@
 from django.shortcuts import render
 from rest_framework import viewsets
-from .serializers import CarSerializer
-from .models import Car
+from rest_framework.decorators import api_view
+from .serializers import CarSerializer, CustomerSerializer
+from .models import Car, Customer
 
 
 class CarsViewSet(viewsets.ModelViewSet):
     queryset = Car.objects.all().order_by('id')
     serializer_class = CarSerializer
+
+
+class CustomersViewSet(viewsets.ModelViewSet):
+    queryset = Customer.objects.all().order_by('id')
+    serializer_class = CustomerSerializer
+
