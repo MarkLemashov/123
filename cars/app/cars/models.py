c1fdc453c3c9d3a6d2637f2fcd446f092f658206
MarkLemashov <marklemashov049@gmail.com>
2020-10-27 15:47:45 +0200

    add car price + purchase validation

--- cars/app/cars/models.py
+++ cars/app/cars/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.db.models.signals import pre_save
 
 
 class Customer(models.Model):
@@ -7,16 +8,36 @@ class Customer(models.Model):
 
     def buy_car(self, car_id):
         car = Car.objects.get(id=car_id)
-        car.customer = self;
-        car.save()
+        self.money -= car.price
+        try:
+            self.save()
+            car.customer = self;
+            car.save()
+        except Exception as error:
+            print(error)
+            self.money = Customer.objects.get(id=self.id).money
+            self.save()
+
+    def earn(self, money_earned):
+        self.money += money_earned
+        self.save()
 
     def __str__(self):
         return self.name
 
 
 class Car(models.Model):
-    make = models.CharField(max_length=60)
+    make = models.CharField(max_length=70)
+    price = models.IntegerField(default=0)
     customer = models.ForeignKey(Customer, on_delete=models.CASCADE, null=True, blank=True)
 
     def __str__(self):
         return self.make
+
+
+def customer_pre_save_validation(sender, instance, *args, **kwargs):
+    if instance.money < 0:
+        raise Exception('Customer does not have enough money.')
+
+
+pre_save.connect(customer_pre_save_validation, sender=Customer)
